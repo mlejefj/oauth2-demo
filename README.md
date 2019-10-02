dae2779a7e57b43fc62fac7fd74c244f4d2816f5
Chris Herdt <cherdt@gmail.com>
2019-10-02 00:12:15 -0500

    Replace app-wide vars with constants

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
-# OAuth 2 Demo
+# OAuth2 Demo
 
 This is based on the demo provided at https://www.oauth.com/oauth2-servers/accessing-data/
 
-I made some small changes to make it work.
+## How to use
 
-## Use
+* Copy `secrets.php.SAMPLE` to `secrets.php`
+* Create a `.gitignore` file and add `secrets.php`: don't save your secrets to GitHub!
+* Visit https://github.com/settings/developers and click _New OAuth App_
+* For _Homepage URL_, enter `http://localhost:8000`
+* For _Authorization Callback URL_, enter `http://localhost:8000/index.php`
+* Edit `secrets.php` and replace the Client ID and Client Secret with the values provided by GitHub
+* Run `php -S localhost:8000`
+* Visit `http://localhost:8000` in your web browser
 
-create .secrets.php
+## Troubleshooting
 
-edit .secrets.php:
+The _logout_ functionality is not implemented. If the application is not behaving as expected, here are some things to try:
 
-    <?php
-    // Fill these out with the values from Github
-    $githubClientID = '';
-    $githubClientSecret = '';
-    ?>
+* Check the messages in the PHP server output. Are there any error messages?
+* The OAuth application in GitHub will allow you to revoke all user tokens
+* Session variables may persist on disk. You can destroy a session by deleting any session files: 
 
-run `php -S 127.0.0.1:8000`
+    rm /var/lib/php/sessions/*
+
+PHP can display the path to stored sessions with the following command:
+
+    echo session_save_path();
--- index.php
+++ index.php
@@ -1,4 +1,33 @@
-<?PHP
+<?php
+
+// load the secrets file
+require('secrets.php');
+
+// Define CONSTANTS (note uppercase), which have global scope
+
+// Send the user to this URL for authorization
+define("AUTHORIZE_URL",     "https://github.com/login/oauth/authorize");
+
+// API endpoint URL to get an access token
+define("TOKEN_URL",         "https://github.com/login/oauth/access_token");
+
+// GitHub's base URL for API requests
+define("API_URL_BASE",      "https://api.github.com/");
+
+// This is the base URL for this app
+define("BASE_URL",          'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['PHP_SELF']);
+
+
+// Start a session so we have a place to
+// store things between redirects
+session_start();
+
+
+// apiRequest: process an API request
+// Takes 3 parameters:
+// - URL
+// - post (boolean): false for GET requests, true for POST
+// - headers (array): HTTP headers
 function apiRequest($url, $post=FALSE, $headers=array()) {
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -20,102 +49,133 @@ function apiRequest($url, $post=FALSE, $headers=array()) {
   return json_decode($response, true);
 }
 
-require('.secrets.php');
 
-// This is the URL we'll send the user to first
-// to get their authorization
-$authorizeURL = 'https://github.com/login/oauth/authorize';
-
-// This is the endpoint we'll request an access token from
-$tokenURL = 'https://github.com/login/oauth/access_token';
-
-// This is the Github base URL for API requests
-$apiURLBase = 'https://api.github.com/';
-
-// The URL for this script, used as the redirect URL
-$baseURL = 'http://' . $_SERVER['SERVER_NAME']
-    . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['PHP_SELF'];
-
-// Start a session so we have a place to
-// store things between redirects
-session_start();
-
-
-// Start the login process by sending the user
-// to Github's authorization page
-if(isset($_GET['action']) && $_GET['action'] == 'login') {
-  unset($_SESSION['access_token']);
+// Process a user login
+//
+// This function will:
+// - Set session state (we will verify on return from GitHub)
+// - Build GET parameters
+// - Redirect to GitHub
+function login() {
 
   // Generate a random hash and store in the session
   $_SESSION['state'] = bin2hex(random_bytes(16));
 
+  // Build GET parameter array
   $params = array(
     'response_type' => 'code',
-    'client_id' => $githubClientID,
-    'redirect_uri' => $baseURL,
+    'client_id' => CLIENT_ID,
+    'redirect_uri' => BASE_URL,
     'scope' => 'user public_repo',
     'state' => $_SESSION['state']
   );
 
   // Redirect the user to Github's authorization page
-  header('Location: '.$authorizeURL.'?'.http_build_query($params));
+  header('Location: ' . AUTHORIZE_URL . '?' . http_build_query($params));
   die();
 }
 
-// When Github redirects the user back here,
-// there will be a "code" and "state" parameter in the query string
-if(isset($_GET['code'])) {
-  // Verify the state matches our stored state
-  if(!isset($_GET['state'])
-    || $_SESSION['state'] != $_GET['state']) {
+// Retrieve an OAuth2 access token
+//
+// This function will:
+// - Verify that the state returned by GitHub matches SESSION state
+// - Make an API request to GitHub with auth code to request access code
+// - Store the access code in the SESSION scope
+// - Redirect to the application home page
+function getAccessToken() {
 
-    header('Location: ' . $baseURL . '?error=invalid_state');
+  // Verify the state matches our stored state
+  if(!isset($_GET['state']) || $_SESSION['state'] != $_GET['state']) {
+
+    header('Location: ' . BASE_URL . '?error=invalid_state');
     die();
   }
 
   // Exchange the auth code for an access token
-  $token = apiRequest($tokenURL, array(
+  $token = apiRequest(TOKEN_URL, array(
     'grant_type' => 'authorization_code',
-    'client_id' => $githubClientID,
-    'client_secret' => $githubClientSecret,
-    'redirect_uri' => $baseURL,
+    'client_id' => CLIENT_ID,
+    'client_secret' => CLIENT_SECRET,
+    'redirect_uri' => BASE_URL,
     'code' => $_GET['code']
   ));
+
+  // Store the access token in the SESSION scope
   $_SESSION['access_token'] = $token['access_token'];
 
-  header('Location: ' . $baseURL);
+  // Redirect user to the main application page
+  header('Location: ' . BASE_URL);
+  // Stop further page processing
   die();
 }
 
-if(isset($_GET['action']) && $_GET['action'] == 'repos') {
-  // Find all repos created by the authenticated user
-  $repos = apiRequest($apiURLBase.'user/repos?'.http_build_query([
-    'sort' => 'created', 'direction' => 'desc'
-  ]));
- 
-  echo '<ul>';
-  foreach($repos as $repo)
-    echo '<li><a href="' . $repo['html_url'] . '">'
-      . $repo['name'] . '</a></li>';
-  echo '</ul>';
+
+// Retrieve GitHub repos and display a formatted HTML list
+//
+// This function will:
+// - Call the GitHub API to request a list of repos
+// - Loop over the returned list and format it as HTML
+function getRepos() {
+    // Find all repos created by the authenticated user
+    $repos = apiRequest(API_URL_BASE . 'user/repos?' . http_build_query([
+      'sort' => 'created', 'direction' => 'desc'
+    ]));
+   
+    echo '<ul>';
+    foreach($repos as $repo)
+      echo '<li><a href="' . $repo['html_url'] . '">'
+        . $repo['name'] . '</a></li>';
+    echo '</ul>';
 }
 
 
-// If there is an access token in the session
-// the user is already logged in
-if(!isset($_GET['action'])) {
+// Display the default application view
+//
+// This function will:
+// - Display a logged in view if an access token is present
+// - Otherwise, provide a link to log in
+function displayDefaultView() {
+
+  // Check for access token in the SESSION scope
   if(!empty($_SESSION['access_token'])) {
     echo '<h3>Logged In</h3>';
     echo '<p><a href="?action=repos">View Repos</a></p>';
-    echo '<p><a href="?action=logout">Log Out</a></p>';
+    echo '<p><a href="?action=logout">Log Out</a> (currently non-functional)</p>';
   } else {
     echo '<h3>Not logged in</h3>';
     echo '<p><a href="?action=login">Log In</a></p>';
-    echo $baseURL;
   }
-  echo session_save_path();
+
+  // Stop further page processing
   die();
 }
 
-?>
 
+// User requests login functionality when:
+// - There is an appropriate GET parameter, e.g. ?action=login
+if(isset($_GET['action']) && $_GET['action'] == 'login') {
+  login();
+}
+
+
+// GitHub redirects the user to the callback URL when:
+// - There are appropriate GET parameters:
+//       - ?code=xxxxx&state=xxxxx
+if(isset($_GET['code']) && isset($_GET['state'])) {
+  getAccessToken();
+}
+
+
+// User requests a list of Git repos when:
+// - There is an appropriate GET parameter:
+//       - ?action=repos
+if(isset($_GET['action']) && $_GET['action'] == 'repos') {
+  getRepos();
+}
+
+
+// User is logged in but no action is requested when:
+// - There are no GET parameters
+if(!isset($_GET['action'])) {
+  displayDefaultView();
+}
